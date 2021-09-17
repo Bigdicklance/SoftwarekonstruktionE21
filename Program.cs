92b022d4b19de3c505e0984992a371afc68f6898
Allan Misasa Nielsen <AMNI1@ucl.dk>
2021-09-17 15:07:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLearn
+{
+    // Person class without abstraction
+    class Person
+    {
+        public int BirthYear;
+        public string FirstName;
+
+        public void age()
+        {
+            int CurrentYear = 2021;
+            int a = CurrentYear - BirthYear;
+            Console.WriteLine(a);
+        }
+
+
+    // Person class with abstraction, inheritance and encapsulation. Can you mention where each OOP pillar exists?
+    abstract class AbstractPerson
+        {
+            public int birthY;
+            public string FirstName;
+            public abstract int MonthlySalary();
+        }
+
+    class ContractWorker : AbstractPerson
+        {
+            public int HourlySalary;
+            public int HoursWorked;
+            public override int MonthlySalary()
+            {
+                int TotalMonthlySalary = HourlySalary * HoursWorked;
+                Console.WriteLine(FirstName + "has a monthly salary of " + TotalMonthlySalary);
+                return TotalMonthlySalary;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Implement the classes
+        }
+    }
+
+}
